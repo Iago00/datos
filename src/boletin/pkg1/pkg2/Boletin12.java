adc501f09340df097cd70ad1417d708da8e39cc2
Iago Pampillón Vieitez <ipampillonvieitez@nautilus27.ad.danielcastelao.org>
2019-10-07 09:05:17 +0200

    primercodigo

---
+++ src/boletin/pkg1/pkg2/Boletin12.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin.pkg1.pkg2;
+
+/**
+ *
+ * @author ipampillonvieitez
+ */
+public class Boletin12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // area
+        float lado = 3;
+        System.out.println("area = "+ 3*3);
+        
+              
+    }
+    
+}
